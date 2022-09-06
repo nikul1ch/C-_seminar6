4c280c1a6a4781b2cb7758a2b4af7c7ef5fd25a0
nikul1ch <nikulin@netkam.ru>
2022-09-06 15:50:03 +0300

    first commit

---
+++ seminar6.1/Program.cs
@@ -0,0 +1,54 @@
+﻿int CountZeros(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+
+Console.Write("Сколько чисел Вы хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+
+Console.WriteLine("Вводит числа после каждого нажимая Enter");
+for (int i = 0; i < n; i++)
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write($"Вы ввели {CountZeros(arr)} чисел больше нуля");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+
+// int[] arr = new int[n];
+
+// void FillingArrayRandom(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-100, 100);
+// }
+
+// int SumOddElements(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i += 2) sum += array[i];  //Счётчик указываю с нуля, т.к. нулевой индекс это 1 позиция)
+//     return sum;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i == array.Length - 1) Console.Write(array[i]);
+//         else Console.Write(array[i] + ", ");
+//     }
+//     Console.WriteLine(" ]");
+// }
+
+// FillingArrayRandom(arr);
+// PrintArray(arr);
+
+// Console.Write("Cумма элементов, стоящих на нечётных позициях: " + SumOddElements(arr));
---
+++ seminar6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"Точка пересечения двух прямых ({x};{y})");
