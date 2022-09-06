dffb19f811b8b03c9a919fe89313924e773d7aa8
nikul1ch <nikulin@netkam.ru>
2022-09-06 15:53:03 +0300

    Программ, которая считает сколько чисел больше нуля ввёл пользователь

--- seminar6.1/Program.cs
+++ seminar6.1/Program.cs
@@ -20,35 +20,3 @@ for (int i = 0; i < n; i++)
 }
 
 Console.Write($"Вы ввели {CountZeros(arr)} чисел больше нуля");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-
-// int[] arr = new int[n];
-
-// void FillingArrayRandom(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-100, 100);
-// }
-
-// int SumOddElements(int[] array)
-// {
-//     int sum = 0;
-//     for (int i = 0; i < array.Length; i += 2) sum += array[i];  //Счётчик указываю с нуля, т.к. нулевой индекс это 1 позиция)
-//     return sum;
-// }
-
-// void PrintArray(int[] array)
-// {
-//     Console.Write("[ ");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (i == array.Length - 1) Console.Write(array[i]);
-//         else Console.Write(array[i] + ", ");
-//     }
-//     Console.WriteLine(" ]");
-// }
-
-// FillingArrayRandom(arr);
-// PrintArray(arr);
-
-// Console.Write("Cумма элементов, стоящих на нечётных позициях: " + SumOddElements(arr));
